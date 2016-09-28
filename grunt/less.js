dd09b0a2c862001a6bf9673d31e4e080e821d287
Kristel van den Akker <kristel.vandenakker@magnaversum.com>
2016-09-28 11:44:53 +0200

    Updated Less grunt file

--- grunt/less.js
+++ grunt/less.js
@@ -13,14 +13,6 @@ module.exports = {
             yuicompress: true,
             optimization: 2
         },
-        files: {
-            'assets/css/comb/styles.css': "assets/css/less/styles.less",
-            'assets/css/comb/shortcodes.css': "assets/css/less/shortcodes.less",
-            'assets/css/comb/grid.css': "assets/css/less/grid.less",
-            'assets/css/comb/layout.css': "assets/css/less/layout.less",
-            'assets/css/comb/print.css': "assets/css/less/print.less",
-            'assets/css/comb/rtl.css': "assets/css/less/rtl.less",
-            'assets/css/comb/temp.css': "assets/css/less/temp.less"
-        }
+        files: {'assets/css/comb/styles.css': "assets/css/less/styles.less"}
     }
 };
