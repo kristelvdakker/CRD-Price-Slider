1f2221ad2ca68fa1778ead253d70194b4b7c142d
Kristel van den Akker <kristel.vandenakker@magnaversum.com>
2016-09-30 17:10:25 +0200

    Form to be able to save to the database

--- crd-price-slider.php
+++ crd-price-slider.php
@@ -119,7 +119,7 @@ function crd_form_buttons($a, $n) {
  * @since 1.0.0
  */
 function crd_form_slider($a, $n, $va) {
-    $id = str_replace(' ', '-', strtolower($n));
+    $id=str_replace(' ', '-', strtolower($n));
     $html = '<div class="option">';
     $html.= '<label class="option_label">' . __($n, 'crd_framework') . ':</label>';
     $html.= '<span id="' . $id . '-product" class="product-name">' . $a[$va - 1] . '</span>';
@@ -153,4 +153,100 @@ function crd_price_slider_page() {
 }
 add_shortcode('crd_price_slider','crd_price_slider_page');
 
-function price_slider_settings_page() {}
+
+
+// create custom plugin settings menu
+add_action('admin_menu', 'crd_create_menu');
+function crd_create_menu() {
+    //create new top-level menu
+    add_menu_page(
+        'My Cool Plugin Settings',
+        'Cool Settings',
+        'administrator',
+        __FILE__,
+        'crd_settings_page',
+        plugins_url('/images/icon.png', __FILE__)
+    );
+}
+
+function crd_settings_page() {
+?>
+<div class="wrap">
+    <h1>Options for the product slider</h1>
+    <form id="product-edit" class="crd-product-form" method="post" action="">
+        <fieldset>
+            <legend><?php _e('CPU and RAM options', 'crd_framework'); ?></legend>
+            <ul>
+                <li class="">
+                    <label for="cpu-options"><?php _e('CPU', 'crd_framework'); ?></label>
+                    <input type="number" min="1" id="cpu-options-field-1" name="cpu-options-field-1" value="<?php echo $args['cpu-options-field-1']; ?>" />
+                    <input type="number" min="1" id="cpu-options-field-2" name="cpu-options-field-2" value="<?php echo $args['cpu-options-field-2']; ?>" />
+                    <input type="number" min="1" id="cpu-options-field-3" name="cpu-options-field-3" value="<?php echo $args['cpu-options-field-3']; ?>" />
+                </li>
+                <li class="">
+                    <label for="ram-options"><?php _e('RAM', 'crd_framework'); ?></label>
+                    <input type="number" id="ram-options-field-1" name="ram-options-field-1" value="<?php echo $args['ram-options-field-1']; ?>" />
+                    <input type="number" id="ram-options-field-2" name="ram-options-field-2" value="<?php echo $args['ram-options-field-2']; ?>" />
+                    <input type="number" id="ram-options-field-3" name="ram-options-field-3" value="<?php echo $args['ram-options-field-3']; ?>" />
+                    <input type="number" id="ram-options-field-4" name="ram-options-field-4" value="<?php echo $args['ram-options-field-4']; ?>" />
+                    <input type="number" id="ram-options-field-5" name="ram-options-field-5" value="<?php echo $args['ram-options-field-5']; ?>" />
+                    <input type="number" id="ram-options-field-6" name="ram-options-field-6" value="<?php echo $args['ram-options-field-6']; ?>" />
+                </li>
+            </ul>
+        </fieldset>
+        <div>
+            <input type="submit" name="submitted" value="<?php _e('Save', 'crd_framework'); ?>" />
+        </div>
+    </form>
+
+    </form>
+</div>
+<?php }
+
+
+
+global $crd_db_version;
+$crd_db_version = '1.0';
+
+function crd_options_install() {
+    global $wpdb;
+    global $crd_db_version;
+
+    $table_name = $wpdb->prefix . 'price_slider';
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        name tinytext NOT NULL,
+        text text NOT NULL,
+        url varchar(55) DEFAULT '' NOT NULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    add_option( 'crd_db_version', $crd_db_version );
+}
+register_activation_hook( __FILE__, 'crd_options_install' );
+
+function crd_options_install_data() {
+    global $wpdb;
+
+    $welcome_name = 'Mr. WordPress';
+    $welcome_text = 'Congratulations, you just completed the installation!';
+
+    $table_name = $wpdb->prefix . 'price_slider';
+
+    $wpdb->insert(
+        $table_name,
+        array(
+            'time' => current_time('mysql'),
+            'name' => $welcome_name,
+            'text' => $welcome_text,
+        )
+    );
+}
+register_activation_hook( __FILE__, 'crd_options_install_data' );
